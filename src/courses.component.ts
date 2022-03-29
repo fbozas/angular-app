b31f977042488a79b9358d501fa06ec6c74717ed
Panos Bozas <fbozas@hotmail.com>
2022-03-29 21:02:22 +0300

    Two way Binding

--- src/courses.component.ts
+++ src/courses.component.ts
@@ -4,10 +4,11 @@ import { CoursesService } from "./app/courses.service";
 @Component({
     selector: 'courses',
     template: `
-    <input #email (keyup.enter)="onKeyUp(email.value)"/>`
+    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>`
 })
 export class CoursesComponent{
-   onKeyUp(email: any){
-        console.log(email);
+   email = "me@gmail.com";
+   onKeyUp(){
+        console.log(this.email);
    }
 }
