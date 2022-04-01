6b27329653848e6a16d25033e0a0e571dcfec727
Panos Bozas <fbozas@hotmail.com>
2022-04-01 21:11:43 +0300

    Angular Material: Buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,8 @@ import {MatSelectModule} from '@angular/material/select';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations'; 
 import {MatInputModule} from '@angular/material/input'; 
 import {ReactiveFormsModule} from '@angular/forms'; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -76,7 +78,9 @@ import {ReactiveFormsModule} from '@angular/forms';
     MatCheckboxModule,
     MatRadioModule,
     MatSelectModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [
     CoursesService,
